a98643cb513ca51448d595f34c80d50dc199493b
chessmonger2112 <tman2kyle@hotmail.com>
2018-07-02 12:25:58 -0700

    Create a start time when program starts.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var dateFormat = require('dateformat');
 
 var currentDay = dateFormat(new Date(), "yyyy-mm-dd");
 var startTime = dateFormat(new Date(), "hh:MM:ss");
+var startDateTime = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
 
 var dayFolder = './' + currentDay;
 
@@ -12,3 +13,22 @@ if (!fs.existsSync(dayFolder)){
 
 var sessionFolder = dayFolder + "/session " + startTime;
 fs.mkdirSync(sessionFolder);
+
+// fs.writeFile(sessionFolder, "Hey there!", function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+
+//     console.log("The file was saved!");
+// });
+
+
+fs.appendFile(sessionFolder + '/startFile.txt', startDateTime, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+
+
+
+//write end time every n seconds
